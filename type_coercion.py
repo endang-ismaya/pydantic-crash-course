30e5799e75f80e3feb7c0635ea89bfe1ad9dc82c
endangismaya <endang.ismaya@gmail.com>
2024-09-30 12:47:16 +0700

    add type_coercion

---
+++ type_coercion.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, ValidationError
+
+
+class Coordinates(BaseModel):
+    x: float
+    y: float
+
+
+p1 = Coordinates(x=1.1, y=2.2)
+p1
+
+# type coercion
+p2 = Coordinates(x=0, y="1.2")
+p2  # Coordinates(x=0.0, y=1.2)
